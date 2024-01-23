72920e58b558ba59859f7253162f7e21466ecd9c
derpygamer2142 <derpygamer2142@gmail.com>
2024-01-22 19:29:31 -0800

    i am so confused, revert all the changes

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 8000
 const { GPU } = require("gpu.js")
+
+app.use("/", express.static(__dirname + "/public"))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
