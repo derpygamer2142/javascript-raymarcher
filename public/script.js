3d376920344399e2a70ea5f0c1499bff18e389bb
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-02-21 11:10:47 -0800

    add the textures for some more characters

--- public/script.js
+++ public/script.js
@@ -64,7 +64,7 @@ async function fetchTextureFromPath(path,width,height) {
 let fov = 135
 let renderDist = 3000;
 let epsilon = 0.01
-let resolution = 16 // works best with a multiple of 2, or just 1
+let resolution = 4 // works best with a multiple of 2, or just 1
 let speed = 25
 let [br,bg,bb] = [7, 237, 218]
 let renderType = "normal" // none, normal, actual, diffuse. None is fastest, diffuse is slowest.
@@ -85,10 +85,10 @@ const textureLibrary = {
 const misc = new Misc()
 const input = new Input()
 const objReader = new objectParser(epsilon)
-let e = await fetch("./models/3dmm models/Rocket/rocket.obj")
+let e = await fetch("./models/3dmm models/Gregory/gregory.obj")
 let model = await e.text()
 
-let output = await objReader.getData(model,0,-35,100,0,0,0,35,textureLibrary.missing,"./models/3dmm models/Rocket/")
+let output = await objReader.getData(model,0,-35,100,0,0,0,35,textureLibrary.missing,"./models/3dmm models/Gregory/")
 console.log(output)
 
 
@@ -97,9 +97,9 @@ let objects = output[1];
 //objects.push(new Sphere(0,0,85,65,0,0,0,0.5,175,heldTexture))
 //objects.push(new Sphere(0,-35,120,75,255,255,255,0.5,175,null))
 //objects.push(new Sphere(0,25,120,60,255,255,255,0.5,175,null))
-//objects.push(new Sphere(0,75,120,45,255,255,255,0.5,175,textureLibrary.duck))
+// objects.push(new Sphere(-35,25,100,55,255,255,255,0.5,175,textureLibrary.duck))
 
-//objects.push(new Sphere(15,75,220,45,255,255,255,0.5,175,textureLibrary.overweight_duck))
+// objects.push(new Sphere(35,25,100,55,255,255,255,0.5,175,textureLibrary.overweight_duck)) // :trol:
 //objects.push(new Box(0,0,185,75,75,75,25,128,127,128,0.5,175,epsilon))
 //objects.push(new Triangle([-45,45,35],[45,-45,35],[-45,-45,35],128,128,128,0.5,175,epsilon,textureLibrary.duck,[0,1],[1,0],[0,0]))
 //objects.push(new Triangle([45,45,35],[45,-45,35],[-45,45,35],128,128,128,0.5,175,epsilon,textureLibrary.duck,[1,1],[1,0],[0,1]))
@@ -113,11 +113,11 @@ let toRender = []
 // let camZ = 140
 // let camXDir = 20
 // let camYDir = -75
-let camX = 0.1 // it breaks when camX is 0. No clue why, not fixing it either.
-let camY = 0
-let camZ = 0
-let camXDir = 0
-let camYDir = 0
+let camX = 2 // it breaks when camX is 0. No clue why, not fixing it either.
+let camY = -120
+let camZ = 160
+let camXDir = 8
+let camYDir = 180
 
 let focalLength = (WIDTH/2)/Math.tan(misc.toRad(fov/2)) // convert FOV to focal length, as that's what the other formulas use. FOV is more human readable tho
 let deltaTime = 0
@@ -283,7 +283,7 @@ async function renderAndUpdate() {
     let renderTime = Date.now() - oldTime
     ctx.fillStyle = "black"
     ctx.font = "30px Comic Sans MS"
-    printLines([`Render load: ${toRender.length} shapes`,`Resolution: ${resolution}`,`Render time: ${renderTime} milliseconds`,`FPS: ${fps.toFixed(3)}`,`DeltaTime: ${deltaTime}`,`Pos: ${camX.toFixed(3)},${camY.toFixed(3)},${camZ.toFixed(3)}`,`Rot: ${camYDir}, ${camXDir}`],0,HEIGHT*0.98,30)
+    //printLines([`Render load: ${toRender.length} shapes`,`Resolution: ${resolution}`,`Render time: ${renderTime} milliseconds`,`FPS: ${fps.toFixed(3)}`,`DeltaTime: ${deltaTime}`,`Pos: ${camX.toFixed(3)},${camY.toFixed(3)},${camZ.toFixed(3)}`,`Rot: ${camYDir}, ${camXDir}`],0,HEIGHT*0.98,30)
     // ctx.fillText(`Rendered in ${renderTime} milliseconds`,0,HEIGHT*0.9)
     // ctx.fillText(`FPS: ${fps.toFixed(3)}`,0,HEIGHT*0.94)
     // ctx.fillText(`DeltaTime: ${deltaTime}`,0,HEIGHT*0.98)
